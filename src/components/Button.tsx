48b33fa3ac7d7a175193de7fa413bbc4508546f7
Elvin Chu <elvincth@gmail.com>
2024-02-27 03:28:16 +0800

    feat: add more components

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -2,16 +2,26 @@ import clsx from "clsx";
 import React from "react";
 
 interface ButtonProps {
+  // The children is a reserved prop in React. It is used to render the content of the component.
+  // See: https://react.dev/reference/react/Children
   children: React.ReactNode;
+  // The className prop is used to add custom classes to the component.
+  // In typescript ? is used to make the prop optional, so it is not required to use it.
   className?: string;
+  // The variant prop is used to change the style of the button, it can be "default" or "outline".
+  // In typescript | is used to define the possible values of the prop.
   variant?: "default" | "outline";
 }
 
+//Here we need to export the Button component to be able to use it in other files.
+//The Button component is a React functional component that receives the ButtonProps as a parameter.
+//The Button component returns a button element with the children as its content.
 export function Button({
-  variant = "default",
-  children,
-  className,
-}: ButtonProps) {
+  variant = "default", // The default value of the variant prop is "default".
+  children, 
+  className, 
+}: ButtonProps) { 
+  // {} is used to destructure the props object, so we can use the props directly without using props.variant, props.children, etc.
   return (
     <button
       className={clsx(
--- src/misc/data.ts
+++ src/misc/data.ts
@@ -1,32 +1,5 @@
 import { IconBrandGithub } from "@tabler/icons-react";
 
-export const resume = [
-  {
-    company: "Local Startup",
-    title: "Intern Software Developer",
-    start: "2023",
-    end: "Present",
-  },
-  {
-    company: "University Coding Club",
-    title: "Member",
-    start: "2021",
-    end: "2023",
-  },
-  {
-    company: "Freelance",
-    title: "Freelance Web Developer",
-    start: "2020",
-    end: "2021",
-  },
-  {
-    company: "The Hong Kong Polytechnic University",
-    title: "BSc in Computing",
-    start: "2019",
-    end: "2023",
-  },
-];
-
 export const projects = [
   {
     name: "CampusNav",
